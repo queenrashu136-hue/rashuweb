f7c15334657b1469a0587b0b6932a8f33a7c158d
queenrashu136-hue <queenrashu136@gmail.com>
2025-11-17 15:37:35 +0530

    Add files via upload

---
+++ mega.js
@@ -0,0 +1,70 @@
+import * as mega from 'megajs';
+
+const auth = {
+    email: 'bonip69328@agenra.com', // Replace with your Mega email 
+    password: 'Rashu#1234567890', // Replace with your Mega password
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+};
+
+export const upload = (data, name) => {
+    return new Promise((resolve, reject) => {
+        try {
+           
+            const storage = new mega.Storage(auth, () => {
+           
+                const uploadStream = storage.upload({ name: name, allowUploadBuffering: true });
+
+           
+                data.pipe(uploadStream);
+
+            
+                storage.on("add", (file) => {
+                    file.link((err, url) => {
+                        if (err) {
+                            reject(err); 
+                        } else {
+                            storage.close();
+                            resolve(url); 
+                        }
+                    });
+                });
+
+            
+                storage.on("error", (error) => {
+                    reject(error);
+                });
+            });
+        } catch (err) {
+            reject(err); 
+        }
+    });
+};
+
+
+export const download = (url) => {
+    return new Promise((resolve, reject) => {
+        try {
+     
+            const file = mega.File.fromURL(url);
+
+            file.loadAttributes((err) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+             
+                file.downloadBuffer((err, buffer) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(buffer); 
+                    }
+                });
+            });
+        } catch (err) {
+            reject(err);
+        }
+    });
+};
+
